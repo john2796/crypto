ed3f3d24f8abb3eb09ec35d521ab306de7fb496d
Mikko Miranda <MikkoMiranda@Apples-MacBook-Pro.local>
2018-09-20 14:40:14 -0700

    Javascript react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
 
 class App extends Component {
+  state = {
+    randomIndex: ""
+  };
+
+  clicked = () => {
+    const randomIndex = Math.floor(Math.random() * 20);
+    this.setState({ randomIndex });
+  };
   render() {
+    const possibleAnswers = [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "siz",
+      "seven",
+      "eight",
+      "nine",
+      "ten"
+    ];
+    const answers = possibleAnswers[this.state.randomIndex];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={this.clicked}>click here to change name</button>
+        {answers}
       </div>
     );
   }
